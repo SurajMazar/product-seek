f60bc2abfc8e0ae9a1587e56c5da5a5e5f582094
Your Name <you@example.com>
2020-12-01 15:47:14 +0545

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter,Route} from 'react-router-dom';
 import './App.css';
+import Wishlist from './Pages/Wishlist/wishlist';
+import Navigation from './components/Navigation/Navigation';
+import Home from './Pages/Home/Home';
+import Stores from './Pages/Store/Store';
+import StoreDetail from './Pages/Store/StoreDetail/StoreDetail';
+import Cart from './Pages/Cart/Cart';
+import Checkout from './Pages/Checkout/Checkout';
+import Dashboard from './Pages/Dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navigation/>
+        {/* pages */}
+        <Route path='/' exact component={Home}/>
+        <Route path='/store' exact component={Stores}/>
+        <Route path='/store/:id'  component={StoreDetail}/>
+        <Route path="/wishlist" component={Wishlist}/>
+        <Route path="/cart" component={Cart}/>
+        <Route path="/checkout" component={Checkout}/>
+        <Route path="/dashboard" component={Dashboard}/>
+        {/* footer */}
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import reducer from './store/reducer';
+import { createStore,applyMiddleware ,compose  } from 'redux';
+import thunk from 'redux-thunk';
+import 'bootstrap/dist/css/bootstrap.css';
+// import "popper.js";
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+require('bootstrap');
 
+require('@fortawesome/fontawesome-free/css/fontawesome.min.css');
+require('@fortawesome/fontawesome-free/css/brands.min.css');
+require('@fortawesome/fontawesome-free/css/regular.min.css');
+require('@fortawesome/fontawesome-free/css/solid.min.css');
+
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer,composeEnhancers(
+  applyMiddleware(thunk)
+));
+
+// import "bootstrap";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> <App /> </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
